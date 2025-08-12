1de874b64c8271aa2e73ad1a38f80189ea68b389
Alc4ntaraa <gabrielaleticia162@gmail.com>
2025-08-12 13:31:35 -0300

    criada as funções iniciais para add e atualizar

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const amigos = []
+
+function adicionarAmigo() {
+    const input = document.getElementById('amigo')
+    const nome = input.value.trim() // trim é usado para retirar os espaços extras
+
+    // se o campo nome ficar vazio, aparece o alerta pedido para fornecer um nome.
+    if (nome === '') {
+        alert('Por favor, digite um nome')
+        return // sai da função para não continuar
+    }
+}
+
+// adiciona o nome no array
+amigos.push(nome)
+// atualiza a lista na tela
+atualizarLista()
+// limpa o campo para o próximo nome
+input.value
+
+function atualizarLista() {
+    const lista = document.getElementById('listaAmigos')
+
+    // limpa a lista para não repetir os nomes
+    lista.innerHTML = ''
+
+    for (let amigo of amigos) {
+        let item = document.createElement("li")
+        item.textContent = amigo
+        lista.appendChild(item)
+    }
+}
+
+
+
+    
