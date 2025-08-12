3e70e06c21b16a17c300cae08372860aa0107e95
Alc4ntaraa <gabrielaleticia162@gmail.com>
2025-08-12 13:57:15 -0300

    resolvendo bug botão adicionar amigo

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const amigos = []
 
 function adicionarAmigo() {
-    const input = document.getElementById('amigo')
+    const input = document.getElementById("amigo")
     const nome = input.value.trim() // trim é usado para retirar os espaços extras
 
     // se o campo nome ficar vazio, aparece o alerta pedido para fornecer um nome.
-    if (nome === '') {
+    if (nome === "") {
         alert('Por favor, digite um nome')
         return // sai da função para não continuar
     }
+    // adiciona o nome no array
+    amigos.push(nome)
+    // atualiza a lista na tela
+    atualizarLista()
+    // limpa o campo para o próximo nome
+    input.value = ""
 }
 
-// adiciona o nome no array
-amigos.push(nome)
-// atualiza a lista na tela
-atualizarLista()
-// limpa o campo para o próximo nome
-input.value
+
 
 function atualizarLista() {
     const lista = document.getElementById('listaAmigos')
@@ -33,7 +34,7 @@ function atualizarLista() {
 
 function sortearAmigo(){
     if (amigos.length == 0) { 
-    alert('Por favor, adicione pelo menos um nome antes de sortear.')
+    alert("Por favor, adicione pelo menos um nome antes de sortear.")
     return // para a função aqui, nada mais acontece
   }
 
